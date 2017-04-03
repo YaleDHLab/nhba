e656d37e605873b220b031eb7c3066d7c123e039
duhaime <douglas.duhaime@gmail.com>
2017-04-03 15:34:49 -0400

    toggle building image and map

--- app/components/Building.jsx
+++ app/components/Building.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BuildingButtons from './building/BuildingButtons'
 import BuildingText from './building/BuildingText'
+import LayoutToggle from './building/BuildingLayoutToggle'
 import Related from './building/Related'
 import Map from './Map'
 import api from '../../config'
@@ -33,12 +34,14 @@ export default class Building extends React.Component {
     super(props)
 
     this.state = {
-      building: {}
+      building: {},
+      layout: {left: 'Map', 'right': 'Gallery'}
     }
 
-    this.getBuilding = this.getBuilding.bind(this)
-    this.processBuilding = this.processBuilding.bind(this)
     this.getStyle = this.getStyle.bind(this)
+    this.getBuilding = this.getBuilding.bind(this)
+    this.toggleLayout = this.toggleLayout.bind(this)
+    this.processBuilding = this.processBuilding.bind(this)
   }
 
   componentDidMount() {
@@ -75,23 +78,41 @@ export default class Building extends React.Component {
     }
   }
 
+  toggleLayout() {
+    const layout = this.state.layout.left == 'Map' ?
+        {left: 'Gallery', right: 'Map'}
+      : {left: 'Map',  right: 'Gallery'}
+    this.setState({layout: layout})
+  }
+
   render() {
+    const style = this.getStyle()
+
+    const layout = {
+      'Map': <Map />,
+      'Gallery': <div className='top-right-top background-image' style={style} />
+    }
+
     return (
       <div className='building'>
         <div className='building-content'>
           <div className='top'>
 
             <div className='left'>
-              <div className='top-left-top'>
-                <Map />
-              </div>
-              <div className='top-left-bottom'>
-                <BuildingButtons fields={fields} {...this.props} />
+              <div className='left-content'>
+                <div className='top-left-top'>
+                  {layout[this.state.layout.left]}
+                </div>
+                <div className='top-left-bottom'>
+                  <LayoutToggle toggleLayout={this.toggleLayout}
+                    layout={this.state.layout} />
+                  <BuildingButtons fields={fields} {...this.props} />
+                </div>
               </div>
             </div>
 
             <div className='right'>
-              <div className='top-right-top background-image' style={this.getStyle()}></div>
+              {layout[this.state.layout.right]}
               <div className='top-right-bottom'>
                 <BuildingText building={this.state.building} fields={fields} />
               </div>
