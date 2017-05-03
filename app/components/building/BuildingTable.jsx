f60f081e5c4690cefed3128d2e3dd7521af16271
duhaime <douglas.duhaime@gmail.com>
2017-05-03 10:45:30 -0400

    improve building table construction logic

--- app/components/building/BuildingTable.jsx
+++ app/components/building/BuildingTable.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
 
-const table = {
-  rows: [
-    [
-      {label: 'Style', field: 'styles'},
-      {label: 'Architect', field: 'architect'}
-    ],
-    [
-      {label: 'Built', field: 'year_built'},
-      {label: 'Client', field: 'client'}
-    ],
-    [
-      {label: 'Use', field: 'current_uses'},
-      {label: 'Current Tenant', field: 'current_tenant'}
-    ],
-    [
-      {label: 'Neighborhood', field: 'neighborhoods'},
-      {label: 'Researcher', field: 'researcher'}
-    ]
-  ]
-}
+const tableFields = [
+  {label: 'Style', field: 'styles'},
+  {label: 'Architect', field: 'architect'},
+  {label: 'Built', field: 'year_built'},
+  {label: 'Client', field: 'client'},
+  {label: 'Use', field: 'current_uses'},
+  {label: 'Current Tenant', field: 'current_tenant'},
+  {label: 'Neighborhood', field: 'neighborhoods'},
+  {label: 'Researcher', field: 'researcher'}
+]
 
 export default class BuildingTable extends React.Component {
   constructor(props) {
@@ -27,28 +17,33 @@ export default class BuildingTable extends React.Component {
   }
 
   render() {
-    let tds = [];
-    table.rows.map((row, i) => {
-      [0,1].map((idx) => {
-        let field = this.props.building[row[idx].field];
-        if ( field && field.length ) {
-          field = _.isArray(field) ? field : [field]
-          tds.push(<td>{row[idx].label}: {field.join(' ')}</td>)
-        }
-      })
+
+    // some tableFields are missing from buildings; ensure
+    // each row in the table has two cells if possible
+
+    let cells = []
+    tableFields.map((f, i) => {
+      let value = this.props.building[f.field];
+      let valueString = Array.isArray(value) ? value.join(' ') : value;
+      if (value) cells.push ( <td key={i}>{f.label}: {valueString}</td> )
+    })
+
+    let rows = [];
+    let rowCells = [];
+    cells.map((c, i) => {
+      rowCells.push(c);
+      if (rowCells.length % 2 == 0) {
+        rows.push(<tr key={i}>{rowCells}</tr>)
+        rowCells = [];
+      }
+
+      if (i+1 === cells.length && rowCells) rows.push(<tr key={i}>{rowCells}</tr>)
     })
 
     return (
       <table className='building-table'>
         <tbody>
-          {table.rows.map((row, i) => {
-            return (
-              <tr key={i}>
-                <td>{row[0].label}: {this.props.building[row[0].field]}</td>
-                <td>{row[1].label}: {this.props.building[row[1].field]}</td>
-              </tr>
-            )
-          })}
+          {rows}
         </tbody>
       </table>
     )
