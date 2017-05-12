5f2c9914ebf25e7926be2e2ad52feb74ca98e34f
duhaime <douglas.duhaime@gmail.com>
2017-05-12 14:53:34 -0400

    restyle admin page for easier use

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,13 +3,28 @@ import React from 'react'
 export default class Footer extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      style: {
+        display: 'table'
+      }
+    }
+  }
+
+  componentWillUpdate() {
+    if (window.location.pathname === '/admin') {
+      if (this.state.style.display != 'table') {
+        const style = {display: 'none'}
+        this.setState({style, style})
+      }
+    }
   }
 
   render() {
     const logo = '/assets/images/dh-wordmark'
 
     return (
-      <footer className='footer'>
+      <footer className='footer' style={this.state.style}>
         <object data={logo + '.svg'} type='image/svg+xml' className='dh-lab-logo'>
           <img src={logo + '.png'} className='logo' />
         </object>
