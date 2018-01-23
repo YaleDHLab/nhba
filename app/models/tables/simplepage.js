b3d70de526bea00828755fb76041aa3ca1a55992
duhaime <douglas.duhaime@gmail.com>
2018-01-22 19:34:29 -0500

    remove auto-increment plugin

--- app/models/tables/simplepage.js
+++ app/models/tables/simplepage.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose')
-var autoIncrement = require('mongoose-auto-increment')
 var mongoosePaginate = require('mongoose-paginate')
 var _ = require('lodash')
 mongoose.Promise = require('bluebird')
@@ -12,8 +11,6 @@ var capitalized = _.startCase(_.toLower(table))
 var schema = new mongoose.Schema(db[table])
 
 // autoincrement a new {{table}}Id field and add pagination
-autoIncrement.initialize(mongoose.createConnection('mongodb://localhost/' + config.db))
-schema.plugin(autoIncrement.plugin, {model: capitalized, field: table + 'Id'})
 schema.plugin(mongoosePaginate)
 
 module.exports = mongoose.model(capitalized, schema, table + 's')
