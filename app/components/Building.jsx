0d606b47252af078c74ecad39b01000c6ddef63d
Eugene Lim <limzhiweieugene@gmail.com>
2018-02-04 15:37:36 -0500

    Remove superfluous conditional

--- app/components/Building.jsx
+++ app/components/Building.jsx
@@ -301,12 +301,11 @@ export default class Building extends React.Component {
               </div>
             </div>
             <div className="right">
-              {this.state.building.images &&
-                this.state.building.images.length > 0 && (
-                  <div className="top-right-top">
-                    {layout[this.state.layout.right]}
-                  </div>
-                )}
+              {this.state.building.images.length > 0 && (
+                <div className="top-right-top">
+                  {layout[this.state.layout.right]}
+                </div>
+              )}
               <div className="top-right-bottom">
                 <BuildingText building={this.state.building} fields={fields} />
               </div>
