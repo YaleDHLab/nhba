7e1c66c1f09a96734a26b53a760de2cea7eedb0a
Eugene Lim <limzhiweieugene@gmail.com>
2018-02-04 22:36:19 -0500

    Restore conditionals

--- app/components/Building.jsx
+++ app/components/Building.jsx
@@ -1,107 +1,109 @@
-import React from 'react'
-import LayoutToggle from './building/BuildingLayoutToggle'
-import BuildingButtons from './building/BuildingButtons'
-import BuildingEditButton from './building/BuildingEditButton'
-import Gallery from './building/BuildingGallery'
-import Related from './building/Related'
-import Loader from './Loader'
-import Map from './Map'
+import React from "react";
+import LayoutToggle from "./building/BuildingLayoutToggle";
+import BuildingButtons from "./building/BuildingButtons";
+import BuildingEditButton from "./building/BuildingEditButton";
+import Gallery from "./building/BuildingGallery";
+import Related from "./building/Related";
+import Loader from "./Loader";
+import Map from "./Map";
 
 // helpers
-import api from '../../config'
+import api from "../../config";
 
 // Building Text components
-import BuildingText from './building/BuildingText'
-import BuildingTextBox from './building/BuildingTextBox'
-import BuildingHistory from './building/BuildingHistory'
-import BuildingStructuralData from './building/BuildingStructuralData'
-import BuildingResources from './building/BuildingResources'
+import BuildingText from "./building/BuildingText";
+import BuildingTextBox from "./building/BuildingTextBox";
+import BuildingHistory from "./building/BuildingHistory";
+import BuildingStructuralData from "./building/BuildingStructuralData";
+import BuildingResources from "./building/BuildingResources";
 
 export default class Building extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       building: {},
-      layout: {left: 'Map', 'right': 'Gallery'},
+      layout: { left: "Map", right: "Gallery" },
       admin: false,
 
       // tour data for mapping
       tourNameToIndex: {}
-    }
+    };
 
-    this.toggleLayout = this.toggleLayout.bind(this)
+    this.toggleLayout = this.toggleLayout.bind(this);
 
     // getter for the text fields for a building
-    this.getTextFields = this.getTextFields.bind(this)
+    this.getTextFields = this.getTextFields.bind(this);
 
     // getter and setter for building data
-    this.getBuilding = this.getBuilding.bind(this)
-    this.processBuilding = this.processBuilding.bind(this)
+    this.getBuilding = this.getBuilding.bind(this);
+    this.processBuilding = this.processBuilding.bind(this);
 
     // getter and setter for user admin status
-    this.checkUserStatus = this.checkUserStatus.bind(this)
-    this.processUserStatus = this.processUserStatus.bind(this)
+    this.checkUserStatus = this.checkUserStatus.bind(this);
+    this.processUserStatus = this.processUserStatus.bind(this);
   }
 
   componentDidMount() {
-    this.getBuilding()
-    this.checkUserStatus()
+    this.getBuilding();
+    this.checkUserStatus();
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.location.query.id != prevProps.location.query.id) {
-      this.getBuilding()
+      this.getBuilding();
     }
   }
 
   /**
-  * Getters and setters for the requested building
-  **/
+   * Getters and setters for the requested building
+   **/
 
   getBuilding() {
     if (this.props.location.query.id) {
       const buildingId = this.props.location.query.id;
-      api.get('buildings?buildingId=' + buildingId, this.processBuilding);
-    } else if (this.props.location.query.random === 'true') {
-      api.get('buildings/random', this.processBuilding);
+      api.get("buildings?buildingId=" + buildingId, this.processBuilding);
+    } else if (this.props.location.query.random === "true") {
+      api.get("buildings/random", this.processBuilding);
     }
   }
 
   processBuilding(err, res) {
-    if (err) { console.warn(err) } else {
-      this.setState({building: res.body[0]})
+    if (err) {
+      console.warn(err);
+    } else {
+      this.setState({ building: res.body[0] });
     }
   }
 
   /**
-  * Check whether the user is an admin or not
-  **/
+   * Check whether the user is an admin or not
+   **/
 
   checkUserStatus() {
-    api.get('session', this.processUserStatus)
+    api.get("session", this.processUserStatus);
   }
 
   processUserStatus(err, res) {
-    if (err) console.warn(err)
+    if (err) console.warn(err);
     if (res.body.session.authenticated === true) {
-      this.setState({admin: true})
+      this.setState({ admin: true });
     }
   }
 
   /**
-  * Allow users to swap building and map positions
-  **/
+   * Allow users to swap building and map positions
+   **/
 
   toggleLayout() {
-    this.state.layout.left === 'Map' ?
-        this.setState({ layout: {left: 'Gallery', right: 'Map'} })
-      : this.setState({ layout: {left: 'Map',  right: 'Gallery'} })
+    this.state.layout.left === "Map"
+      ? this.setState({ layout: { left: "Gallery", right: "Map" } })
+      : this.setState({ layout: { left: "Map", right: "Gallery" } });
   }
 
   /**
-  * Retrieve the fields required for creating building table and action buttons
-  **/
+   * Retrieve the fields required for creating building table and action buttons
+   **/
 
   getTextFields() {
     const building = this.state.building;
@@ -109,164 +111,187 @@ export default class Building extends React.Component {
 
     const fields = [
       {
-        label: 'Overview',
-        button: {label: 'Overview', icon: 'overview'},
-        href: 'description',
+        label: "Overview",
+        button: { label: "Overview", icon: "overview" },
+        href: "description",
         collapsible: false,
-        contentFields: [ 'overview_description' ],
-        component: <BuildingTextBox title='Overview'
-          text={building.overview_description} />
+        contentFields: ["overview_description"],
+        component: (
+          <BuildingTextBox
+            title="Overview"
+            text={building.overview_description}
+          />
+        )
       },
       {
-        label: 'Physical Description',
-        button: {label: 'Physical Description', icon: 'building'},
-        href: 'physical-description',
+        label: "Physical Description",
+        button: { label: "Physical Description", icon: "building" },
+        href: "physical-description",
         collapsible: false,
-        contentFields: [ 'physical_description' ],
-        component: <BuildingTextBox title='Physical Description'
-          text={building.physical_description} />
+        contentFields: ["physical_description"],
+        component: (
+          <BuildingTextBox
+            title="Physical Description"
+            text={building.physical_description}
+          />
+        )
       },
       {
-        label: 'Urban Setting',
-        button: {label: 'Urban Setting', icon: 'building'},
-        href: 'urban-setting',
+        label: "Urban Setting",
+        button: { label: "Urban Setting", icon: "building" },
+        href: "urban-setting",
         collapsible: false,
-        contentFields: [ 'urban_setting' ],
-        component: <BuildingTextBox title='Urban Setting'
-          text={building.urban_setting} />
+        contentFields: ["urban_setting"],
+        component: (
+          <BuildingTextBox
+            title="Urban Setting"
+            text={building.urban_setting}
+          />
+        )
       },
       {
-        label: 'Social History',
-        button: {label: 'Social History', icon: 'building'},
-        href: 'social-history',
+        label: "Social History",
+        button: { label: "Social History", icon: "building" },
+        href: "social-history",
         collapsible: false,
-        contentFields: [ 'social_history' ],
-        component: <BuildingTextBox title='Social History'
-          text={building.social_history} />
+        contentFields: ["social_history"],
+        component: (
+          <BuildingTextBox
+            title="Social History"
+            text={building.social_history}
+          />
+        )
       },
       {
-        label: 'Site History',
-        button: {label: 'Site History', icon: 'building'},
-        href: 'site-history',
+        label: "Site History",
+        button: { label: "Site History", icon: "building" },
+        href: "site-history",
         collapsible: false,
-        contentFields: [ 'site_history' ],
-        component: <BuildingTextBox title='Site History'
-          text={building.site_history} />
+        contentFields: ["site_history"],
+        component: (
+          <BuildingTextBox title="Site History" text={building.site_history} />
+        )
       },
       {
-        label: 'Structural Data',
-        button: {label: 'Structural Data', icon: 'structure'},
-        href: 'structuralData',
+        label: "Structural Data",
+        button: { label: "Structural Data", icon: "structure" },
+        href: "structuralData",
         component: <BuildingStructuralData building={building} />,
         collapsible: true,
         contentFields: [
-          'historic_uses',
-          'street_visibilities',
-          'dimensions',
-          'materials',
-          'roof_types',
-          'structural_conditions',
-          'past_tenants',
-          'accessibilities',
-          'levels',
-          'structures',
-          'roof_materials'
+          "historic_uses",
+          "street_visibilities",
+          "dimensions",
+          "materials",
+          "roof_types",
+          "structural_conditions",
+          "past_tenants",
+          "accessibilities",
+          "levels",
+          "structures",
+          "roof_materials"
         ]
       },
       {
-        label: 'Resources',
-        button: {label: 'Resources', icon: 'community'},
-        href: 'resources',
+        label: "Resources",
+        button: { label: "Resources", icon: "community" },
+        href: "resources",
         component: <BuildingResources building={building} />,
         collapsible: true,
-        contentFields: [
-          'archive_documents',
-          'sources'
-        ]
+        contentFields: ["archive_documents", "sources"]
       }
     ];
 
     // only return fields if one or more of their contentFields are populated
     // in the current building
     let extantFields = [];
-    fields.map((field) => {
+    fields.map(field => {
       let kept = false;
-      field.contentFields.map((contentField) => {
+      field.contentFields.map(contentField => {
         if (building[contentField] && building[contentField].length && !kept) {
           extantFields.push(field);
           kept = true;
         }
-      })
-    })
+      });
+    });
 
     return extantFields;
   }
 
   /**
-  * Main render function
-  **/
+   * Main render function
+   **/
 
   render() {
     const building = this.state.building;
-    const location = building && building.latitude && building.longitude ?
-        {lat: parseFloat(building.latitude), lng: parseFloat(building.longitude)}
-      : null
+    const location =
+      building && building.latitude && building.longitude
+        ? {
+            lat: parseFloat(building.latitude),
+            lng: parseFloat(building.longitude)
+          }
+        : null;
 
-    const map = (
-      this.state.tourNameToIndex && building ?
-        <Map buildings={[this.state.building]}
+    const map =
+      this.state.tourNameToIndex && building ? (
+        <Map
+          buildings={[this.state.building]}
           tourNameToIndex={this.state.tourNameToIndex}
-          initialLocation={location} />
-      : <Loader />
-    )
+          initialLocation={location}
+        />
+      ) : (
+        <Loader />
+      );
 
     const layout = {
-      'Map': map,
-      'Gallery': <Gallery
-        building={this.state.building}
-        layout={this.state.layout} />
-    }
+      Map: map,
+      Gallery: (
+        <Gallery building={this.state.building} layout={this.state.layout} />
+      )
+    };
 
-    const fields = this.getTextFields()
+    const fields = this.getTextFields();
 
     return (
-      <div className='building'>
-        <div className='building-content'>
-          <div className='top'>
-            <div className='left'>
-              <div className='left-content'>
-                <div className='top-left-top'>
+      <div className="building">
+        <div className="building-content">
+          <div className="top">
+            <div className="left">
+              <div className="left-content">
+                <div className="top-left-top">
                   {layout[this.state.layout.left]}
                 </div>
-                <div className='top-left-bottom'>
+                <div className="top-left-bottom">
                   <LayoutToggle
                     toggleLayout={this.toggleLayout}
-                    layout={this.state.layout} />
-                  <BuildingButtons
-                    fields={fields} {...this.props} />
+                    layout={this.state.layout}
+                  />
+                  <BuildingButtons fields={fields} {...this.props} />
                   <BuildingEditButton
                     admin={this.state.admin}
-                    building={this.state.building} />
+                    building={this.state.building}
+                  />
                 </div>
               </div>
-              <div className='bottom'>
-                <h1 className='label'>Related Buildings</h1>
+              <div className="bottom">
+                <h1 className="label">Related Buildings</h1>
                 <Related building={this.state.building} />
               </div>
             </div>
-            <div className='right'>
-              <div className='top-right-top'>
-                {layout[this.state.layout.right]}
-              </div>
-              <div className='top-right-bottom'>
-                <BuildingText
-                  building={this.state.building}
-                  fields={fields} />
+            <div className="right">
+              {this.state.building.images &&
+                this.state.building.images.length > 0 && (
+                  <div className="top-right-top">
+                    {layout[this.state.layout.right]}
+                  </div>
+                )}
+              <div className="top-right-bottom">
+                <BuildingText building={this.state.building} fields={fields} />
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router'
+import React from "react";
+import { Link } from "react-router";
 
 export default class Card extends React.Component {
   constructor(props) {
-    super(props)
-  
-    this.getStyle = this.getStyle.bind(this)
-    this.handleClick = this.handleClick.bind(this)
-    this.scrollTo = this.scrollTo.bind(this)
+    super(props);
+
+    this.getStyle = this.getStyle.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.scrollTo = this.scrollTo.bind(this);
   }
 
   getStyle() {
-    const dir = '/assets/uploads/resized/small';
-    const filename = this.props.building.images.length > 0 ?
-        this.props.building.images[0].filename
-      : 'NA'
-    const url = dir + '/' + filename;
-    return {backgroundImage: 'url(' + url + ')'}
+    const style =
+      this.props.building.images && this.props.building.images.length > 0
+        ? {
+            backgroundImage: `url(/assets/uploads/resized/small/${
+              this.props.building.images[0].filename
+            })`
+          }
+        : {
+            backgroundColor: "#f1f1f1"
+          };
+    return style;
   }
 
   handleClick(e) {
@@ -24,20 +29,21 @@ export default class Card extends React.Component {
   }
 
   /**
-  * Function to smooth scroll on a page
-  *
-  * @args:
-  *   {int} to: the y position of document to which we should scroll
-  *   {int} duration: the duration of the scroll
-  * @returns:
-  *   none
-  **/
+   * Function to smooth scroll on a page
+   *
+   * @args:
+   *   {int} to: the y position of document to which we should scroll
+   *   {int} duration: the duration of the scroll
+   * @returns:
+   *   none
+   **/
 
   scrollTo(to, duration) {
     const self = this;
     if (duration <= 0) return;
     const doc = document.documentElement;
-    const scrolled = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    const scrolled =
+      (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
     const difference = to - scrolled;
     const perTick = difference / duration * 10;
 
@@ -52,39 +58,42 @@ export default class Card extends React.Component {
     const building = this.props.building;
 
     // parse out the large and small card text
-    let bigtext = '',
-        smalltext = '';
+    let bigtext = "",
+      smalltext = "";
 
     if (building.building_name) {
       bigtext = building.building_name;
       smalltext = building.address;
     } else {
       bigtext = building.address;
-      if (bigtext && bigtext.includes(', New Haven')) {
-        let splitaddress = bigtext.split(', New Haven');
+      if (bigtext && bigtext.includes(", New Haven")) {
+        let splitaddress = bigtext.split(", New Haven");
         bigtext = splitaddress[0];
 
         // parse out the small text
-        if (splitaddress[1].substring(0,1) == ',') {
-          smalltext = 'New Haven, ' + splitaddress[1].substring(1).trim();
+        if (splitaddress[1].substring(0, 1) == ",") {
+          smalltext = "New Haven, " + splitaddress[1].substring(1).trim();
         } else {
-          smalltext = 'New Haven, ' + splitaddress[1].trim();
+          smalltext = "New Haven, " + splitaddress[1].trim();
         }
       }
     }
 
     return (
-      <div className='card' onClick={this.handleClick}>
-        <Link to={'/building?id=' + this.props.building._id + '#'}>
-          <div className='card-content'>
-            <div className='background-image card-image' style={this.getStyle()} />
-            <div className='card-text'>
-              <div className='card-name'>{bigtext}</div>
-              <div className='card-details'>{smalltext}</div>
+      <div className="card" onClick={this.handleClick}>
+        <Link to={"/building?id=" + this.props.building._id + "#"}>
+          <div className="card-content">
+            <div
+              className="background-image card-image"
+              style={this.getStyle()}
+            />
+            <div className="card-text">
+              <div className="card-name">{bigtext}</div>
+              <div className="card-details">{smalltext}</div>
             </div>
           </div>
         </Link>
       </div>
-    )
+    );
   }
 }
